b4c000fa4747d3f394df5278a634951d2184b374
AGanchev19 <130899140+ganchev-alex@users.noreply.github.com>
2023-04-16 13:01:30 +0300

    Creating the Firm struct.
    This it the main struct that will be used when going over the file. Here the struct is defined with all of its fields with the corresponding data type.

---
+++ headers/firm-structure.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+typedef struct {
+    
+    unsigned int rowID;
+    char orderID[14];
+    char orderDate[10];
+    char customer[8];
+    char city[50];
+    char state[50];
+    unsigned int postalCode;
+    char region[10];
+    char productID[15];
+    char catgory[35];
+    char subCategory[25];
+    unsigned int price; 
+
+} Firm;
+
